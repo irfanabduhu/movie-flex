2c559991d179984e1c6d02a6e878b97b822f2598
Irfanul Hoque <irhq13@gmail.com>
2023-02-25 19:42:04 +0600

    Enabled cors to accept request from frontend

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const sequelize = require("./config/database");
 require("./models/association");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
